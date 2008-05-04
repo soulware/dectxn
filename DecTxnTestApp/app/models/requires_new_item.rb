b56c25658c16f7ce3404077a71e3ac9e9a8e5b07
soulware <soulware@4a9433e1-d360-4096-8ce1-a46ac5279aa0>
2008-05-04 18:43:17 +0000

    requires_new_items
    git-svn-id: svn+ssh://rubyforge.org/var/svn/dectxn/trunk@33 4a9433e1-d360-4096-8ce1-a46ac5279aa0

--- DecTxnTestApp/app/models/requires_new_item.rb
+++ DecTxnTestApp/app/models/requires_new_item.rb
@@ -1,2 +1,8 @@
 class RequiresNewItem < ActiveRecord::Base
+  def change_price(price)
+    self.price = price    
+    save!
+    
+    throw Exception.new("requires_new is not currently implemented")
+  end
 end
