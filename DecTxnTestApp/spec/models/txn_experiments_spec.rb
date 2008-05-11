ab88b0aed634f78a40ec6e44a796433a9d119f16
soulware <soulware@4a9433e1-d360-4096-8ce1-a46ac5279aa0>
2008-05-11 17:57:36 +0000

    new tests
    git-svn-id: svn+ssh://rubyforge.org/var/svn/dectxn/trunk@56 4a9433e1-d360-4096-8ce1-a46ac5279aa0

--- DecTxnTestApp/spec/models/txn_experiments_spec.rb
+++ DecTxnTestApp/spec/models/txn_experiments_spec.rb
@@ -14,4 +14,54 @@ describe Item do
   
     Thread.current['open_transactions'].should == 0
   end
+  
+  it "should increment open_transactions twice" do
+    Item.new.transaction do
+      Item.new.transaction do
+        Thread.current['open_transactions'].should == 2
+      end
+    end
+  end
+  
+  it "should increment open_transactions twice" do
+    User.new.transaction do
+      Item.new.transaction do
+        Thread.current['open_transactions'].should == 2
+      end
+    end
+  end
+  
+  it "should end with 0 open_transactions" do
+    Item.new.transaction do
+      Item.new.transaction do
+        # empty
+      end
+    end
+  
+    Thread.current['open_transactions'].should == 0
+  end
+ 
+  it "should end with 0 open_transactions" do
+    Item.new.transaction do
+      User.new.transaction do
+        # empty
+      end
+    end
+  
+    Thread.current['open_transactions'].should == 0
+  end
+ 
+  it "should set start_db_transaction == true" do
+    Item.new.transaction do
+      Thread.current['start_db_transaction'].should == true
+    end
+  end 
+  
+  it "should set start_db_transaction == false" do
+    Item.new.transaction do
+      Item.transaction do
+        Thread.current['start_db_transaction'].should == false
+      end
+    end
+  end
 end
